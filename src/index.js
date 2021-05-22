66539856efeb3f09cf89b53ef4440edda8374195
Halyna Sinko <galinasinko14@gmail.com>
2021-05-23 00:08:15 +0300

    Создает шаблон элементов меню и добавляет их в DOM.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+
+import menuItemsTpl from './templates/menu-items.hbs';
+import menuData from './menu.json';
 import './styles.css';
+
+const refs = {
+    menuList: document.querySelector('.js-menu'),
+};
+
+function createMenuItems(menuData) {
+    
+    return menuItemsTpl(menuData);
+};
+
+const menuMarkup = createMenuItems(menuData);
+console.log(menuMarkup);
+
+refs.menuList.insertAdjacentHTML('beforeend', menuMarkup);
